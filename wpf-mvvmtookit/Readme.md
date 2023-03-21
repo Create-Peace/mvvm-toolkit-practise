f585ac87f9c6b4d20a0cbb70fe7369cabd35a0cf
Ming <455747495@qq.com>
2023-03-21 23:23:07 +0800

    init

---
+++ wpf-mvvmtookit/Readme.md
@@ -0,0 +1,4 @@
+﻿
+
+
+https://www.cnblogs.com/1024E/p/16622483.html
---
+++ wpf-mvvmtookit/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_mvvmtookit.Models;
+
+namespace wpf_mvvmtookit.ViewModels
+{
+    public class MainViewModel
+    {
+        //在MianViewModel中写处理逻辑，这个类 类似于在不分mvvm模式下的后台的代码
+        public CalculateModel? calculateModel { get; set; }
+        public MainViewModel()
+        {
+            calculateModel = new CalculateModel();
+            calculateModel.a = 1;
+            calculateModel.b = 1;
+            calculateModel.c = calculateModel.a + calculateModel.b;
+        }
+    }
+}
